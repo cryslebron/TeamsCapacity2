2b5a3aa12dcf49bb621df4a8096e48f4f54b8efd
cryslebron <cryslebron@yahoo.com>
2020-07-20 21:23:25 -0400

    testing heroku deployment

--- api/app.js
+++ api/app.js
@@ -4,7 +4,7 @@ const sequelize = require('./util/database')
 
 const app = express()
  
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 
 app.use('/api', routes.security)
 app.use('/api/team', routes.teams)
