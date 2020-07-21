7500d96f18232418cee97f591541eb6b4fddc6d3
cryslebron <cryslebron@yahoo.com>
2020-07-20 22:51:28 -0400

    testing heroku deployment3

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const routes = require('./routes/index')
+const sequelize = require('./util/database')
 const app = express()
 const port = process.env.PORT || 3001;
 
@@ -20,4 +21,15 @@ app.use((err, req, res, next) => {
     res.send('500 - Server Error')
 })
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+sequelize
+    .sync()
+    .then(result => {
+        console.log(result);
+        app.listen(port, () => console.log(
+            `Express started in ${app.get('env')} mode at http://localhost:${port}; ` +
+            `press Ctrl-C to terminate`
+        ))
+    })
+    .catch(err => {
+        console.log(err);        
+    })
